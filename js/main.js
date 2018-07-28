2aade1a78cb59ba3a2e384f84437422501f8e67a
Carlos Mañas <carlos@spacenomads.com>
2018-07-28 19:36:45 +0200

    Add complete game functionality

--- js/main.js
+++ js/main.js
@@ -3,12 +3,20 @@
 const settings = document.querySelectorAll('.pairs__option-item');
 const goBTN = document.querySelector('.pairs__go');
 const cards = document.querySelector('.pairs__cards');
+const pauseLayer = document.querySelector('.pairs__overlay');
 
 const maxVisibleCards = 2
 let visibleCards = 0;
+const pairs = {
+  a: 0,
+  b: 0
+};
 
 const resetGame = () => {
   cards.innerHTML = '';
+  visibleCards = 0;
+  pairs.a = 0;
+  pairs.b = 0;
 };
 
 
@@ -24,19 +32,45 @@ const hideCards = () => {
   const cards = document.querySelectorAll('.card--is-visible');
   for (const c of cards) {
     c.classList.remove('card--is-visible');
-    visibleCards = 0;
   }
+  visibleCards = 0;
+  pauseLayer.classList.remove('pairs__overlay--active');
 };
 
 
 const cardCounter = c => {
   visibleCards += parseInt(c);
-  console.log(`cartas visibles: ${visibleCards}`);
 }
 
 
+const disableCards = (pairID) => {
+  const cards = document.querySelectorAll('.card');
+  for (const c of cards) {
+    if (c.getAttribute('data-pair') === pairID) {
+      c.removeEventListener('click', playCard);
+      c.classList.add('card--disabled');
+    }
+  }
+  visibleCards = 0;
+  pauseLayer.classList.remove('pairs__overlay--active');
+};
+
+
 const checkVisiblePair = () => {
+  pauseLayer.classList.add('pairs__overlay--active');
   console.log('comprobando parejas');
+  console.log(`cartas visibles: ${visibleCards}`);
+  console.log(`carta A: ${pairs.a}`);
+  console.log(`carta B: ${pairs.b}`);
+  if (pairs.a === pairs.b) {
+    console.log('IGUALES!!!');
+    disableCards(pairs.a);
+  } else {
+    console.log('Todo mal');
+    //mostrar overflow
+    setTimeout(hideCards, 1000);
+  }
+  console.log('***')
 };
 
 
@@ -49,14 +83,21 @@ const playCard = e => {
   } else {
     if (visibleCards === maxVisibleCards) {
       hideCards();
+    } else {
+      card.classList.add('card--is-visible');
+      cardCounter(1);
+      if (visibleCards == 1) {
+        pairs.a = card.getAttribute('data-pair');
+      } else {
+        pairs.b = card.getAttribute('data-pair');
+      }
+      if (visibleCards === maxVisibleCards) {
+        checkVisiblePair();
+      }
     }
-    card.classList.add('card--is-visible');
-    cardCounter(1);
+
   }
 
-  if (visibleCards === maxVisibleCards) {
-    checkVisiblePair();
-  }
 
 }
 
@@ -67,6 +108,7 @@ const initGame = (data) => {
 
     const cardItem = document.createElement('div');
     cardItem.className = 'card';
+    cardItem.setAttribute('data-pair', card.pair);
     cardItem.addEventListener('click', playCard);
 
     const cardFront = document.createElement('div');
