815bd02b81dc047553c09d2e6f47f0fea842a9a9
Carlos Mañas <carlos@spacenomads.com>
2018-07-28 17:12:01 +0200

    add basic game interface

--- js/main.js
+++ js/main.js
@@ -1,3 +1,36 @@
 'use strict';
 
-console.log('>> Ready :)');
+const settings = document.querySelectorAll('.pairs__option-item');
+const goBTN = document.querySelector('.pairs__go');
+const cards = document.querySelector('.pairs__cards');
+
+const resetGame = () => {
+  cards.innerHTML = '';
+}
+
+
+const checkSettings = options => {
+  for (const option of options) {
+    if (option.checked) {
+      return option.value;
+    }
+  }
+}
+
+const beginGame = () => {
+  resetGame();
+
+  const cardsURL = 'https://raw.githubusercontent.com/Adalab/cards-data/master/' + checkSettings(settings) + '.json';
+
+  fetch(cardsURL)
+    .then(response => {
+      const gameData = response.json();
+
+      console.log(gameData);
+    })
+}
+
+goBTN.addEventListener('click', beginGame);
+
+
+
